cafe4473050ff941270f9a1f7fe8d093cb159431
Adrien <adrien@arccoza.com>
2017-11-21 13:28:54 +0900

    Add tests for `isUpperCase` and `isLowerCase`

--- test/kase.test.js
+++ test/kase.test.js
@@ -1,7 +1,7 @@
 const test = require('tape')
 const print = console.log.bind(console)
 const tapDiff = require('tap-diff')
-const {kase} = require('../lib/index')
+const {kase, isUpperCase, isLowerCase} = require('../lib/index')
 
 
 if(!module.parent) {
@@ -31,6 +31,36 @@ of cases repeatedly, without introducing artifacts.`, function (t) {
       t.equal(kase(fix[from], from, to), fix[to])
     }
   }
+
+  t.end()
+})
+
+test(`isUpperCase should return true if all chars are uppercase, \
+false if even one char is lowercase.`, function (t) {
+  t.ok(isUpperCase('TEST CASE'))
+  t.notOk(isUpperCase('TEST CaSE'))
+  t.notOk(isUpperCase('test case'))
+  t.ok(kase.isUpperCase('TEST CASE'))
+  t.notOk(kase.isUpperCase('TEST CaSE'))
+  t.notOk(kase.isUpperCase('test case'))
+  t.ok(kase.isUpper('TEST CASE'))
+  t.notOk(kase.isUpper('TEST CaSE'))
+  t.notOk(kase.isUpper('test case'))
+
+  t.end()
+})
+
+test(`isLowerCase should return true if all chars are lowercase, \
+false if even one char is uppercase.`, function (t) {
+  t.ok(isLowerCase('test case'))
+  t.notOk(isLowerCase('test cAse'))
+  t.notOk(isLowerCase('TEST CASE'))
+  t.ok(kase.isLowerCase('test case'))
+  t.notOk(kase.isLowerCase('test cAse'))
+  t.notOk(kase.isLowerCase('TEST CASE'))
+  t.ok(kase.isLower('test case'))
+  t.notOk(kase.isLower('test cAse'))
+  t.notOk(kase.isLower('TEST CASE'))
 
   t.end()
 })
