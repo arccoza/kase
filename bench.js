01255f6ca71aabc2bbb41af5b8a14cc9593f22dd
Adrien <adrien@arccoza.com>
2017-12-08 15:32:52 +0900

    Adhock test changes

--- bench.js
+++ bench.js
@@ -1,4 +1,5 @@
 var {kase} = require('./lib/index')
+var {Rexi} = require('./lib/rexi')
 var print = console.log.bind(console)
 
 
@@ -15,18 +16,43 @@ let fix = {
 }
 
 
-var total = 0
-for (let from in fix) {
-  var start = process.hrtime()
-  for (var i = 0; i < 10000; i++) {
-    for (let to in fix) {
-      kase(fix[from], from, to)
-    }
-  }
-  var end = process.hrtime(start)
-  // total += end
-  print(from)
-  print(end)
-}
+// for (let from in fix) {
+//   console.time('kase')
+//   for (var i = 0; i < 10000; i++) {
+//     for (let to in fix) {
+//       kase(fix[from], 'any', to)
+//     }
+//   }
+//   console.timeEnd('kase')
+// }
 
-print(total)
+// console.time('kase')
+// for (var i = 0; i < 100000; i++) {
+//   kase('tCTTTe', 'any', 'kebab')
+// }
+// console.timeEnd('kase')
+
+print(kase('@@@-----CTTTeTmpppLateYYYYYY_YYYYi8_8', 'camel', 'kebab'))
+print(kase('@@@-----CTt-te-tmppp-late-yYYYYY_YYy-yi8-8', 'kebab', 'title'))
+print(kase('@@@-----CTt Te Tmppp Late YYYYYY_YYy Yi8 8', 'title', 'pascal'))
+print(kase('@@@-----CTtTeTmpppLateYYYYYY_YYyYi8_8', 'pascal', 'header'))
+
+// print(kase('-', 'any', 'camel'))
+
+// var reStr = `((?<foo>aa))|(bc)(?<foo>.)`
+// var re = new Rexi(reStr, 'g')
+// var re2 = new RegExp(`((aa))|(bc)(.)`, 'g')
+// // var it = re.iterator('aaaa')
+// // print(it.next(), it.next(), it.next(), it.next())
+// // print(re.exec('aabcd'), re.exec('aaaa'), re.exec('aaaa'))
+// // print(re.replace('aabcd', 'b'), re.lastIndex)
+// console.time('Rexi')
+// var start = process.hrtime()
+// for (var i = 0; i < 100000; i++) {
+//   var res = re.exec('aabcd')
+//   // var res = 'aabcd'.replace(re, 'b')
+// }
+// print(res)
+// var end = process.hrtime(start)
+// console.timeEnd('Rexi')
+// print(end)
